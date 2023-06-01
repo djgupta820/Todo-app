51e42de7faf03ddc349ab48da7221bcbd68b1259
Dheeraj Gupta <djgupta820@gmail.com>
2023-06-01 16:04:40 +0530

    Login and register page created (frontend)

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+app.get('/register', (req,res)=>{
+    res.render('register')
+})
+
+app.listen('5000', (req,res)=>{
+    console.log('server running at http://localhost:5000')
+})
