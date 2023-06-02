964917f91e063f71982a31a3c297fc7ee8cb3659
Dheeraj Gupta <djgupta820@gmail.com>
2023-06-02 15:07:43 +0530

    All features created

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const crypto = require('crypto')
 
 const app = express()
-
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -24,27 +24,43 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
+const eventSchema = new mongoose.Schema({
+    user: String,
+    title: String,
+    time: String,
+    description: String
+})
+
 const User = new mongoose.model('User', userSchema)
+const Event = new mongoose.model('Event', eventSchema)
+
+var _user = {}
 /* ----------------------------------------------------------------------- */
 
 app.get('/', (req,res)=>{
-    res.render('index')
+    res.render('index', {message: ""})
 })
 
 app.post('/login', (req,res)=>{
     const {username, password} = req.body
-    User.find({$and: [{username: username}, {password: password}]}).then((msg)=>{
+    let algorithm = 'sha256'
+    let key = password
+    let digest = crypto.createHash(algorithm).update(key).digest('hex')
+    User.find({$and: [{username: username}, {password: digest}]}).then((msg)=>{
         if(msg.length > 0){
-            let user = User.find({$and: [{username: username}, {password: password}]})
-            res.render('dashboard', {user})
+            let user = msg[0]
+            _user = user
+            let message = "Login Successfull :)"
+            res.render('dashboard', {user, message})
         }else{
             console.log('login failed :(')
-            // let message = "Invalid Username or Password"
-            res.redirect('/')
+            let message = "Invalid Username or Password"
+            res.render('index', {message})
         }
     }).catch((err)=>{
         console.log(err)
-        res.redirect('/')
+        let message = "Error"
+        res.render('index', {message})
     })
 })
 
@@ -54,23 +70,85 @@ app.get('/register', (req,res)=>{
 
 app.get('/registerUser', (req,res)=>{
     const {name, username, email, password} = req.query
-    console.log(name,username, email, password)
+    let algorithm = 'sha256'
+    let key = password
+    let digest = crypto.createHash(algorithm).update(key).digest('hex')
     User.create([{
         name: name,
         username: username,
         email: email, 
-        password: password
+        password: digest
     }]).then((msg)=>{
-        console.log('User registered successfull :)')
-        res.redirect('/')
+        let message = 'Registration Successfull! :)'
+        res.render('index', {message})
     }).catch((err)=>{
-        console.log('Error registering user :(')
-        res.redirect('/')
+        let message = 'Error registering user :('
+        res.render('register', {message})
     })
 })
 
 app.get('/dashboard', (req,res)=>{
-    res.render('dashboard')
+    res.render('dashboard', {user: _user, message: "Welcome to Dashboard"})
+})
+
+app.get('/profile', (req,res)=>{
+    res.render('profile', {user:_user})
+})
+
+app.get('/addEvent', (req,res)=>{
+    const message = ""
+    res.render('addEvent', {user: _user, message: message})
+})
+
+app.get('/add-event', (req,res)=>{
+    const {title, time, description} = req.query
+    Event.create({
+        user: _user.username,
+        title: title,
+        time: time,
+        description: description
+    }).then((msg)=>{
+        const message = "Event Created Successfully! :)"
+        const type = "success"
+        res.render('addEvent', {user: _user, message:message, type:type})
+    }).catch((err)=>{
+        const message = "Couldn't Create Successfully! :)"
+        const type = "danger"
+        res.render('addEvent', {user: _user, message:message, type:type})  
+    })
+})
+
+app.get('/allEvents', (req,res)=>{
+    Event.find({ user: _user.username }).then((msg)=>{
+        const message = ''
+        console.log(msg)
+        res.render('allEvents', {user: _user, events:msg, message:message})
+    }).catch((err)=>{
+        console.log(err)
+    })
+})
+
+app.get('/deleteEvent/:eventid', (req,res)=>{
+    const {eventid} = req.params
+    console.log(eventid)
+    Event.findByIdAndDelete({_id: eventid}).then((msg)=>{
+        Event.find({ user: _user.user }).then((msg)=>{
+            const message = "Event Deleted! "
+            const type = 'success'
+            res.render('allEvents', {user: _user, events:msg, message:message, type:type})
+        }).catch((err)=>{
+            const message = "Couldn't Delete! "
+            const type = 'danger'
+            res.render('allEvents', {user: _user, events:msg, message:message, type:type})
+        })
+    }).catch((err)=>{
+        console.log(err)
+    })
+})
+
+app.get('/logout', (req,res)=>{
+    console.log(_user)
+    res.render('logout')
 })
 
 app.listen('5000', (req,res)=>{
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+let editButton = document.querySelector('#edit-profile')
